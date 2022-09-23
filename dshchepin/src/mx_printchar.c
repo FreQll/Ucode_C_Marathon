9d60e3faf6a610c4b0e192ff259a4c858c38c9ef
FreQl <62791316+Dyylando@users.noreply.github.com>
2022-09-23 13:32:15 +0300

    Delete dshchepin directory

--- dshchepin/src/mx_printchar.c
+++
@@ -1,6 +0,0 @@
-#include "../inc/header.h"
-
-void mx_printchar(char c) {
-    write(1, &c, 1);
-}
-
--- dshchepin/src/mx_printerr.c
+++
@@ -1,8 +0,0 @@
-#include "../inc/header.h"
-
-int mx_strlen(const char *s);
-
-void mx_printerr(const char *s) {
-    write(2, s, mx_strlen(s));
-}
-
